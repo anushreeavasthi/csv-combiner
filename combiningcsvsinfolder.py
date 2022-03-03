92fa0ba1f49cbae4e03b36ad9fcac377f84e2996
anushreeavasthi <33330562+anushreeavasthi@users.noreply.github.com>
2022-03-03 01:45:13 -0500

    command line arguments changes

--- combiningcsvsinfolder.py
+++ combiningcsvsinfolder.py
@@ -1,27 +1,43 @@
+import csv
 import os
 import sys
 import glob
+from numpy import std
 import pandas as pd
 
-#Mentioning the source directory as a command line argument
-source_dir= str(sys.argv[1])
 
-#Passing path to the source directory
-path = source_dir
+def combine_pdf (input,output):
+    #Mentioning the source directory as a command line argument
+    source_dir= str(input)
+    path = source_dir
+    files_collection = glob.glob(os.path.join(path, "*.csv"))
+    combined_dataframe = []
+    for file in files_collection:
+        df = pd.read_csv(file, sep=',')
+        df['filename'] = file.split('/')[-1]
+        combined_dataframe.append(df)
+    master_df = pd.concat(combined_dataframe, ignore_index=True, sort=True)
+    files_collection = glob.glob(os.path.join(path, "*.csv"))
+    combined_dataframe = []
+    for file in files_collection:
+        df = pd.read_csv(file, sep=',')
+        df['filename'] = file.split('/')[-1]
+        combined_dataframe.append(df)
+    master_df = pd.concat(combined_dataframe, ignore_index=True, sort=True)
+    return(master_df.to_csv(output,index=False))
 
-#Creating a parser for all files in the folder which end with .csv
-files_collection = glob.glob(os.path.join(path, "*.csv"))
 
-combined_dataframe = []
-for file in files_collection:
-    df = pd.read_csv(file, sep=',')
-    df['filename'] = file.split('/')[-1]
-    combined_dataframe.append(df)
+combine_pdf(sys.argv[1],"temp.csv")
+
+
+with open("temp.csv", 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        print(row)
+
+
+    
   
-master_df = pd.concat(combined_dataframe, ignore_index=True, sort=True)
-
-#Saving df as csv
-master_df.to_csv("temporary.csv", index=False)
 
 
 
--- combiningcsvwithfilenames.py
+++ combiningcsvwithfilenames.py
@@ -1,38 +1,40 @@
+import csv
 import os
 import sys
 import glob
+from numpy import std
 import pandas as pd
 
-#total number of arguments
-n = len(sys.argv)
-print("Total arguments passed:", n)
-
-#Checking all arguments passed and storing in a list
-print("\nArguments passed:", end = " ")
-arguments=[]
-files_collection=[]
-combined_dataframe = []
-for i in range(1, n):
-    print(sys.argv[i], end = " ")
-    arguments.append(sys.argv[i])
 
 
-for i in arguments:
-    path=arguments[i]
-    print(path)
-
+#Mentioning the source directory as a command line argument
+source_dir= str(input)
+path = source_dir
 files_collection = glob.glob(os.path.join(path, "*.csv"))
-
+combined_dataframe = []
 for file in files_collection:
     df = pd.read_csv(file, sep=',')
     df['filename'] = file.split('/')[-1]
     combined_dataframe.append(df)
-
-
-
 master_df = pd.concat(combined_dataframe, ignore_index=True, sort=True)
+files_collection = glob.glob(os.path.join(path, "*.csv"))
+combined_dataframe = []
+for file in files_collection:
+    df = pd.read_csv(file, sep=',')
+    df['filename'] = file.split('/')[-1]
+    combined_dataframe.append(df)
+master_df = pd.concat(combined_dataframe, ignore_index=True, sort=True)
+master_df.to_csv("temporary.csv",index=False)
 
-#Saving df as csv
-master_df.to_csv('Master.csv', index=False)
+
+with open("temp.csv", 'r') as csvfile:
+    datareader = csv.reader(csvfile)
+    for row in datareader:
+        print(row)
+
+
+    
+  
+
 
 
